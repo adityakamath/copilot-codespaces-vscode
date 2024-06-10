6a6de51bff98af72114d8ec38daddd9a92d27691
Aditya Kamath <adityakamath@live.com>
2024-06-10 09:38:17 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,47 +1,39 @@
-// create web server
-// create a web server that listens for requests on port 3000
-// when a request is received, read the file comments.json
-// send the contents of the file to the client
-// if the file is not found, send a 404 status code
+// create simple web server
+// run with command: node comments.js
+// visit http://localhost:3000/ in your browser
 
-// require http module
+// Require modules
 var http = require('http');
-// require fs module
-var fs = require('fs');
-// require path module
-var path = require('path');
+var url = require('url');
+var qs = require('querystring');
+var comments = [];
 
-// create server
-var server = http.createServer(function (req, res) {
-  // if request is a GET request
-  if (req.method === 'GET') {
-    // if request is for /comments
-    if (req.url === '/comments') {
-      // get the comments.json file
-      fs.readFile(path.join(__dirname, 'comments.json'), function (err, data) {
-        if (err) {
-          // if file is not found, send 404 status code
-          res.writeHead(404);
-          res.end();
-          return;
-        }
-        // send the contents of the file to the client
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(data);
-      });
+// Create server
+http.createServer(function(req, res) {
+    // Get the URL and parse it
+    var parsedUrl = url.parse(req.url, true);
+    // Get the path
+    var path = parsedUrl.pathname;
+    // Get the query string as an object
+    var query = parsedUrl.query;
+
+    // Handle the request
+    if (path == '/add' && req.method == 'POST') {
+        var body = '';
+        req.on('data', function(data) {
+            body += data;
+        });
+        req.on('end', function() {
+            var comment = qs.parse(body).comment;
+            comments.push(comment);
+            res.end('Comment added');
+        });
+    } else if (path == '/comments' && req.method == 'GET') {
+        res.end(JSON.stringify(comments));
     } else {
-      // if request is not for /comments, send 404 status code
-      res.writeHead(404);
-      res.end();
+        res.writeHead(404);
+        res.end('Not found');
     }
-  } else {
-    // if request is not a GET request, send 404 status code
-    res.writeHead(404);
-    res.end();
-  }
-});
+}).listen(3000);
 
-// listen for requests on port 3000
-server.listen(3000, function () {
-  console.log('Server listening on port 3000');
-});
+console.log('Server running at http://localhost:3000/');
